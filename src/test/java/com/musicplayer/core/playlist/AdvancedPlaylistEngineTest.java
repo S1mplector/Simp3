eda4cf573c8306dfba4e4e19400172f22a6775aa
i6385148 <mehmetogluilgaz07@gmail.com>
2025-09-17 17:14:53 +0200

    test: add repeat all mode wrap-around behavior test for playlist engine

--- src/test/java/com/musicplayer/core/playlist/AdvancedPlaylistEngineTest.java
+++ src/test/java/com/musicplayer/core/playlist/AdvancedPlaylistEngineTest.java
@@ -33,4 +33,29 @@ public class AdvancedPlaylistEngineTest {
         // And history should not grow because we didn't advance tracks
         assertTrue(engine.getHistory().isEmpty(), "History should remain empty when repeating the same song");
     }
+
+    @Test
+    void repeat_all_wraps_to_start_and_updates_history() {
+        // Given a playlist of three songs
+        Song s1 = new Song(1, "One", "A", "Al", "G", 180, "/a/1.mp3", 1, 2001);
+        Song s2 = new Song(2, "Two", "B", "Al", "G", 200, "/a/2.mp3", 2, 2002);
+        Song s3 = new Song(3, "Three", "C", "Al", "G", 220, "/a/3.mp3", 3, 2003);
+        List<Song> list = Arrays.asList(s1, s2, s3);
+
+        AdvancedPlaylistEngine engine = new AdvancedPlaylistEngine();
+        engine.setPlaylist(list);
+        engine.setCurrentIndex(2); // current = s3 (end of playlist)
+        engine.setRepeatMode(PlaylistEngine.RepeatMode.ALL);
+
+        // When next() is called at end with Repeat ALL, it should wrap
+        Song next = engine.next();
+
+        // Then we should be at the first song
+        assertEquals(s1, next, "Repeat ALL should wrap to first song after last");
+        assertEquals(0, engine.getCurrentIndex(), "Index should wrap to 0 in Repeat ALL mode");
+
+        // History should record the previously current song (s3) at the front
+        assertFalse(engine.getHistory().isEmpty(), "History should contain previous song");
+        assertEquals(s3, engine.getHistory().get(0), "Most recent history entry should be the previously current song");
+    }
 }
